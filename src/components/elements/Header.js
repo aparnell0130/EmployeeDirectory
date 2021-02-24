89319f86f0df22446565c3eb79069c8746cc01eb
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-24 15:21:58 -0800

    created header with jumbotron

---
+++ src/components/elements/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Employee Directory</h1>
+                    <p className="lead">Easily navigate through your Employees</p>
+                </div>
+            </div>
+        </div>
+    )
+}
