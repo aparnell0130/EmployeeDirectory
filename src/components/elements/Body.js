581222c794f45205ef04c6ea55d19a63678053f3
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-24 15:35:22 -0800

    added bootstrap to index.js for styling

--- src/components/elements/Body.js
+++ src/components/elements/Body.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Body() {
     return (
-        <div>
+        <div className='container'>
             <ul className="list-group">
                 <li className="list-group-item">An item</li>
                 <li className="list-group-item">A second item</li>
