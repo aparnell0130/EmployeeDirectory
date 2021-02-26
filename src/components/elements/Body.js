25b45cb2ebf86ecff7bd862ecfd0a6a46367d79e
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-25 20:27:42 -0800

    added input for search

--- src/components/elements/Body.js
+++ src/components/elements/Body.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-export default function Body({ users }) {
+export default function Body({ users, search }) {
     return (
         <div className='container'>
+            <div className="form-group">
+                <label for="employee">Search Employee</label>
+                <input type="text" className="form-control" id="employee" placeholder="Start searching employee name" onChange={search} />
+            </div>
             <ul className="list-group">
                 {users.map(user => (
                     <li key={user.email} className='list-group-item'>
