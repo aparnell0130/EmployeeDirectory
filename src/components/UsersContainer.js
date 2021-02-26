bba17a9ff4bae938b4d30cba0b46e1461f4c50a5
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-02-25 16:17:58 -0800

    added componentDidMount with api function

--- src/components/UsersContainer.js
+++ src/components/UsersContainer.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import API from '../utils/API'
 import Body from './elements/Body'
 import Header from './elements/Header'
 
 export class UsersContainer extends Component {
+    state = {
+        users: []
+    }
+    componentDidMount() {
+        API().then(res =>
+            this.setState({
+                users: res.results
+            })).catch(err => console.log(err))
+    }
     render() {
         return (
             <div>
                 <Header />
-                <Body />
+                <Body users={this.state.users} />
             </div>
         )
     }
